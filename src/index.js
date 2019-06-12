be8b2ed1cd77d32fd6cef227538bff63b8e8c3a7
Daniel Delgado <38217821+daniel-delgado99@users.noreply.github.com>
2019-06-12 03:26:19 -0300

    Correção

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const con = mysql.createConnection({
 
 const database = "FknqGegRiP";
 
+
 // =========================================
 //      Login
 // =========================================
